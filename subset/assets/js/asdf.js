aaad81dea905781a30481ec0c557f173d98d9599
LeeChenSe <42361526+LiChenZe@users.noreply.github.com>
2020-06-18 15:45:42 +0800

    2020-06-18

---
+++ subset/assets/js/asdf.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-18 14:12:16
+ * @LastEditTime: 2020-06-18 14:12:56
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \LiChenZe.github.io\subset\assets\js\asdf.js
+ */ 
+
+
+ window.onload = function () {
+    
+    $(function () {
+
+        /**
+         * 设置WeChat点击模态框
+         */
+        function weChatModalBox() {
+            var weChatImg = createElement('img');
+            weChatImg.setAttribute('src', '../../assets/images/wechatImg.png');
+            weChatImg.style.cssText = ""
+            document.body.appendChild(weChatImg);
+        }
+
+    })
+ }
