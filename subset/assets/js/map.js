45d54693cecb61042bf90fa3b980b0ee65a716c3
leechenze <leeczyc@gmail.com>
2023-10-07 14:38:22 +0800

    Modify some text information for each page

--- subset/assets/js/map.js
+++ subset/assets/js/map.js
@@ -4,98 +4,31 @@
 
 
 function initMap() {
-// Styles a map in night mode.
-var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 25.762055, lng: -80.191802 },
-    zoom: 12,
-    styles: [
-    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-    {
-        featureType: "administrative.locality",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }]
+  mapboxgl.accessToken = 'pk.eyJ1IjoibGVlY2hlbnplIiwiYSI6ImNsOHZib3lwYTBjOW0zb3M4N21mNXZ0d2kifQ.PG5zSa0rC0abFSIEW-Zpcw';
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-97.92515697819557, 39.383402222450854], // starting position [lng, lat]
+    zoom: 1 // starting zoom
+  });
+  // Create a default Marker and add it to the map.
+  const marker = new mapboxgl.Marker()
+    .setLngLat([108.94236945482766, 34.26103164881724])
+    .addTo(map);
+  map.flyTo({
+    center: [108.94236945482766, 34.26103164881724],
+    zoom: 9,
+    speed: 0.3,
+    curve: 1,
+    easing(t) {
+      return t;
     },
-    {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }]
-    },
-    {
-        featureType: "poi.park",
-        elementType: "geometry",
-        stylers: [{ color: "#263c3f" }]
-    },
-    {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#6b9a76" }]
-    },
-    {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [{ color: "#38414e" }]
-    },
-    {
-        featureType: "road",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#212a37" }]
-    },
-    {
-        featureType: "road",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#9ca5b3" }]
-    },
-    {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [{ color: "#746855" }]
-    },
-    {
-        featureType: "road.highway",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#1f2835" }]
-    },
-    {
-        featureType: "road.highway",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#f3d19c" }]
-    },
-    {
-        featureType: "transit",
-        elementType: "geometry",
-        stylers: [{ color: "#2f3948" }]
-    },
-    {
-        featureType: "transit.station",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }]
-    },
-    {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [{ color: "#17263c" }]
-    },
-    {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#515c6d" }]
-    },
-    {
-        featureType: "water",
-        elementType: "labels.text.stroke",
-        stylers: [{ color: "#17263c" }]
-    }
-    ]
-});
-
-var marker = new google.maps.Marker({
-    position: { lat: 25.762055, lng: -80.191802 },
-    map: map,
-    title: "Bangalore"
-});
+    maxDuration: 99999,
+    essential: true // this animation is considered essential with respect to prefers-reduced-motion
+  });
 }
 
+initMap();
 
 // END: Custom Map 
